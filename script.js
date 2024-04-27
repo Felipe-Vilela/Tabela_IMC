2e6234423328b6fb6399bfe693f737516a927525
Felipe Vilela <felipe.vilela01@outlook.com>
2024-04-27 11:45:07 -0300

    Atualização dos eventos

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-//FUNÇÔES
-// function calculoImc(peso, altura) {
-// 	let resultado = peso / (altura * altura);
-
-// 	return resultado;
-// }
-
-// function statusImc(resultadoCalculo) {
-// 	let status;
-
-// 	if (resultadoCalculo < 18.50) {
-// 			status = "Magreza";
-// 	} else if (resultadoCalculo <= 24.99) {
-// 			status = "Saudável";
-// 	} else if (resultadoCalculo <= 29.99) {
-// 			status = "Sobrepeso";
-// 	} else if (resultadoCalculo >= 30.00 && resultadoCalculo <= 34.99) {
-// 			status = "Obesidade I";
-// 	} else if (resultadoCalculo >= 35.00 && resultadoCalculo <= 39.99) {
-// 			status = "Obesidade II";
-// 	} else if (resultadoCalculo >= 40.00) {
-// 			status = "Obesidade III";
-// 	}
-
-// 	return status
-// }
-
 function verificaDados(nome, peso, altura) {
 	if (peso <= 0 || altura <= 0 || nome === "") {
 			return false;
@@ -42,60 +15,61 @@ function atualizarTabela() {
 							throw new Error("Falha ao carregar os dados.");
 					}
 					return resposta.json();
-			})
-			.then((dados) => {
-					const tbody = document.querySelector("#corpo-tabela");
-					limparTabela() // Limpa o conteúdo do tbody antes de adicionar os novos dados
-					
-					for (let pessoa of dados) {
-						let trPessoa = document.createElement("tr");
-						let tdNome = document.createElement("td");
-						let tdAltura = document.createElement("td");
-						let tdPeso = document.createElement("td");
-						let tdImc = document.createElement("td");
-						let tdStatus = document.createElement("td");
-						let tdOpcao = document.createElement("td");
+	})
+	.then((dados) => {
+		const tbody = document.querySelector("#corpo-tabela");
+		limparTabela() // Limpa o conteúdo do tbody antes de adicionar os novos dados
+		
+		for (let pessoa of dados) {
+			let trPessoa = document.createElement("tr");
+			let tdNome = document.createElement("td");
+			let tdAltura = document.createElement("td");
+			let tdPeso = document.createElement("td");
+			let tdImc = document.createElement("td");
+			let tdStatus = document.createElement("td");
+			let tdOpcao = document.createElement("td");
 
-						let btnMenosPeso = document.createElement("button");
-						let btnMaisPeso = document.createElement("button");
-						let btnExcluir = document.createElement("button");
+			let btnMenosPeso = document.createElement("button");
+			let btnMaisPeso = document.createElement("button");
+			let btnExcluir = document.createElement("button");
 
-						// Adicionando Classes nos btn
-						btnExcluir.classList.add("botaos_tabela_excluir");
-						btnMaisPeso.classList.add("botaos_tabela", "mais_peso");
-						btnMenosPeso.classList.add("botaos_tabela", "menos_peso");
+			// Adicionando Classes nos btn
+			btnExcluir.classList.add("botaos_tabela_excluir");
+			btnMaisPeso.classList.add("botaos_tabela", "mais_peso");
+			btnMenosPeso.classList.add("botaos_tabela", "menos_peso");
 
-						// Insere os botões das opções na tabela
-						btnMaisPeso.innerText = "+ Peso";
-						btnMenosPeso.innerText = "- Peso";
-						btnExcluir.innerText = "Excluir";
+			// Insere os botões das opções na tabela
+			btnMaisPeso.innerText = "+ Peso";
+			btnMenosPeso.innerText = "- Peso";
+			btnExcluir.innerText = "Excluir";
 
-						tdOpcao.append(btnMaisPeso);
-						tdOpcao.append(btnMenosPeso);
-						tdOpcao.append(btnExcluir);
+			tdOpcao.append(btnMaisPeso);
+			tdOpcao.append(btnMenosPeso);
+			tdOpcao.append(btnExcluir);
 
-						tdNome.innerText = pessoa.nome;
-						tdAltura.innerText = pessoa.altura;
-						tdPeso.innerText = pessoa.peso;
-						tdImc.innerText = pessoa.imc;
-						tdStatus.innerText = pessoa.status;
+			tdNome.innerText = pessoa.nome;
+			tdAltura.innerText = pessoa.altura;
+			tdPeso.innerText = pessoa.peso;
+			tdImc.innerText = pessoa.imc;
+			tdStatus.innerText = pessoa.status;
 
-						trPessoa.append(tdNome);
-						trPessoa.append(tdAltura);
-						trPessoa.append(tdPeso);
-						trPessoa.append(tdImc);
-						trPessoa.append(tdStatus);
-						trPessoa.append(tdOpcao);
+			trPessoa.append(tdNome);
+			trPessoa.append(tdAltura);
+			trPessoa.append(tdPeso);
+			trPessoa.append(tdImc);
+			trPessoa.append(tdStatus);
+			trPessoa.append(tdOpcao);
 
-						tbody.appendChild(trPessoa); // Adiciona a linha à tabela
-					
-					}
-				}
-					
-			)
-			.catch(error => {
-					console.error("Error: " + error);
-			});
+			tbody.appendChild(trPessoa); // Adiciona a linha à tabela
+
+			addEventoMaisPeso(btnMaisPeso, pessoa.id, pessoa.peso);
+			addEventoMenosPeso(btnMenosPeso, pessoa.id, pessoa.peso);
+			addEventoExcluirLinha(btnExcluir, pessoa.id)
+		}
+	})
+	.catch(error => {
+			console.error("Error: " + error);
+	});
 }
 
 function limparTabela() {
@@ -109,42 +83,38 @@ function adicionarPessoa() {
 	let nome = document.querySelector("#nome").value;
 	let altura = document.querySelector("#altura").value;
 	let peso = document.querySelector("#peso").value;
-	// let imc = parseFloat(calculoImc(peso, altura)).toFixed(2);
-	// let status = statusImc(imc);
+
 
 	if (verificaDados(nome, peso, altura)) {
-			const url = "https://ifsp.ddns.net/webservices/imc/pessoa";
-			
-			let options = {
-					method: "POST",
-					body: JSON.stringify({
-							nome: nome,
-							altura: altura,
-							peso: peso,
-							// imc: imc,
-							// status: status,
-					}),
-					headers: {
-							"Content-type": "application/json"
-					}
-			}
-			fetch(url, options).then((resposta) => {
-					if (!resposta.ok) {
-							throw new Error("Falha ao adicionar pessoa.");
-					}
-					return resposta.json();
-					})
-					.then(() => {
-							limparTabela();
-							atualizarTabela();
-					})
-					.catch(error => console.error("Error:" + error));
-			} else {
-					e.preventDefault();
-					alert("Há campos vazios ou dados inválidos. Peso e Altura precisam ser maiores que 0.");
-			}
+		const url = "https://ifsp.ddns.net/webservices/imc/pessoa";
+		
+		let options = {
+				method: "POST",
+				body: JSON.stringify({
+						nome: nome,
+						altura: altura,
+						peso: peso,
+				}),
+				headers: {
+						"Content-type": "application/json"
+				}
+		}
+		fetch(url, options).then((resposta) => {
+				if (!resposta.ok) {
+						throw new Error("Falha ao adicionar pessoa.");
+				}
+				return resposta.json();
+				})
+				.then(() => {
+						limparTabela();
+						atualizarTabela();
+				})
+				.catch(error => console.error("Error:" + error));
+		} else {
+				e.preventDefault();
+				alert("Há campos vazios ou dados inválidos. Peso e Altura precisam ser maiores que 0.");
+		}
 	};
-
 
 function addEventoMaisPeso(btnMaisPeso, idPessoa, pesoPessoa) {
 	btnMaisPeso.addEventListener("click", () => {
@@ -215,11 +185,12 @@ function addEventoMenosPeso(btnMenosPeso, idPessoa, pesoPessoa) {
 }
 
 
-function addEventoExcluir(btnExcluir, idPessoa) {
+function addEventoExcluirLinha(btnExcluir, idPessoa) {
 	btnExcluir.addEventListener("click", () => {
 		const url = "https://ifsp.ddns.net/webservices/imc/pessoa/" + idPessoa;
 
 		fetch(url, {
+			method: "DELETE",
 			
 		}).then((resposta) => {
 			if (!resposta.ok) {
@@ -237,69 +208,107 @@ function addEventoExcluir(btnExcluir, idPessoa) {
 	});	
 }
 
-// function pegarId() {
-// 	const url = "https://ifsp.ddns.net/webservices/imc/pessoa";
+function addRemoverMaiorImc(){
+	const url = "https://ifsp.ddns.net/webservices/imc/pessoa";
+	let maiorImc;
+	let idMaiorImc;
+	
+	fetch(url)
+	.then((resposta) => {
+			if (!resposta.ok) {
+					throw new Error("Falha ao carregar os dados.");
+			}
+			return resposta.json();
+	})
+	.then((dados) => {
+		maiorImc = dados[0].imc;
+		idMaiorImc = dados[0].id;
+		for (let pessoa of dados) {
+			if (maiorImc < pessoa.imc) {
+				maiorImc = pessoa.imc
+				idMaiorImc = pessoa.id
+			}
+		}
+		addEventoExcluir(idMaiorImc)
+	}		
+	)
+	.catch(error => {
+			console.error("Error: " + error);
+	});
+}
 
-// 	fetch(url).then((resposta) => {
-// 		if (!resposta.ok) {
-// 			throw new Error("Falha ao carregar os dados.");
-// 		}
-// 		return resposta.json();
-// 	}).then((dados) => {
-// 		let btnRemover = document.querySelectorAll(".botaos_tabela_excluir");
-		
-// 		for(let i = 0; i < btnRemover.length; i++){
-// 			// Associe o ID do objeto JSON ao botão como um atributo de dados
-// 			// console.log(btnRemover[i].dataset.id)
 
-// 			// btnRemover[i].addEventListener("click", function() {
-// 			// 	// Recupere o ID do objeto JSON a partir do atributo de dados do botão
-// 			// 	let id = this.dataset.id;
-// 			// 	addEventoExcluir(id)
-// 			// });
-// 		}
-// 	}).catch(error => {
-// 		console.error("Error: ", error);
-// 	});
-// }
+function addRemoverMenorImc(){
+	const url = "https://ifsp.ddns.net/webservices/imc/pessoa";
+	let menorImc;
+	let idMenorImc;
+	
+	fetch(url)
+	.then((resposta) => {
+			if (!resposta.ok) {
+					throw new Error("Falha ao carregar os dados.");
+			}
+			return resposta.json();
+	})
+	.then((dados) => {
+		menorImc = dados[0].imc;
+		idMenorImc = dados[0].id;
+		for (let pessoa of dados) {
+			if (menorImc > pessoa.imc) {
+				menorImc = pessoa.imc
+				idMenorImc = pessoa.id
+			}
+		}
+		addEventoExcluir(idMenorImc)
+	}		
+	)
+	.catch(error => {
+			console.error("Error: " + error);
+	});
+}
+
+
+function addEventoExcluir(idPessoa) {
+		const url = "https://ifsp.ddns.net/webservices/imc/pessoa/" + idPessoa;
+
+		fetch(url, {
+			method: "DELETE",
+			
+		}).then((resposta) => {
+			if (!resposta.ok) {
+				throw new Error("Falha ao carregar os dados.");
+			}
+			return resposta.json();
+		})
+		.then((dados) => {
+			limparTabela();
+			atualizarTabela();
+		})
+		.catch(error => {
+			console.error("Error: " + error);
+		});
+}
 
 function main() {
 	atualizarTabela();
 
 	let btnPost = document.querySelector("#post");
-
-
 	btnPost.addEventListener("click", (e) => {
 		e.preventDefault();
 		adicionarPessoa()
 	})
 
-	
+	let btnMaiorImc = document.querySelector("#btnRemoverMaiorImc");
+	let btnMenorImc = document.querySelector("#btnRemoverMenorImc");
 
-	addEventoExcluir()
-
-	
-	// let btnRemover = document.querySelectorAll(".botaos_tabela_excluir");
-
-	// for(let btn of btnRemover){
-	// 	btn.addEventListener("click", () => {
-	// 		alert("Foi")
-	// 	})
+	btnMaiorImc.addEventListener("click", () => {
+		addRemoverMaiorImc()
 		
-	// }
-	
-	
+	})
 
-
-
-	// pegarId()
-
-
-
-
-	// if (btnPost === true) {
-	// 	adicionarPessoa();
-	// }
+	btnMenorImc.addEventListener("click", () => {
+		addRemoverMenorImc()
+	})
 
 }
 
